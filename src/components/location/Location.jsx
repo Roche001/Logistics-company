d44188321f58e3895a910112df72614c6ccbac10
Roche001 <rocheharold13@gmail.com>
2022-05-27 08:34:42 +0300

    update changes

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -29,65 +29,67 @@ const Location = () => {
           <h3>Where to Find Us</h3>
         </div>
       </div>
-      <div className="location-container">
-        <div className="location-contacts">
-          <h2>Nairobi</h2>
-          <div className="call-one">
-            <h4>
-              325 City Square <br /> Westland,NRB 95678
-            </h4>
-            <p>
-              Phone: 916.358.5622 <br />
-              Open hours <br />
-              Mon - Fri: 7:00 am - 8:00 pm <br />
-              Sat: 8:00 am - 3:00 pm <br />
-              Sun: Closed
-            </p>
+      <div className="location-bg">
+        <div className="location-container">
+          <div className="location-contacts">
+            <h2>Nairobi</h2>
+            <div className="call-one">
+              <h4>
+                325 City Square <br /> Westland,NRB 95678
+              </h4>
+              <p>
+                Phone: 916.358.5622 <br />
+                Open hours <br />
+                Mon - Fri: 7:00 am - 8:00 pm <br />
+                Sat: 8:00 am - 3:00 pm <br />
+                Sun: Closed
+              </p>
+            </div>
+            <div className="call-two">
+              <a href="Button" className="click">
+                REQUEST A CALLBACK
+              </a>
+            </div>
+            <div className="call-three">
+              <a href="button" className="click-up">
+                Contact us
+              </a>
+            </div>
           </div>
-          <div className="call-two">
-            <a href="Button" className="click">
-              REQUEST A CALLBACK
-            </a>
-          </div>
-          <div className="call-three">
-            <a href="button" className="click-up">
-              Contact us
-            </a>
+          <div className="location-contacts">
+            <h2>Cape Town</h2>
+            <div className="call-one">
+              <h4>
+                146 Freedom Square <br /> Mandela Drive,CPT 95678
+              </h4>
+              <p>
+                Phone: 916.358.5622 <br />
+                Open hours <br />
+                Mon - Fri: 7:00 am - 8:00 pm <br />
+                Sat: 8:00 am - 3:00 pm <br />
+                Sun: Closed
+              </p>
+            </div>
+            <div className="call-two">
+              <a href="Button" className="click">
+                REQUEST A CALLBACK
+              </a>
+            </div>
+            <div className="call-three">
+              <a href="button" className="click-up">
+                Contact us
+              </a>
+            </div>
           </div>
         </div>
-        <div className="location-contacts">
-          <h2>Cape Town</h2>
-          <div className="call-one">
-            <h4>
-              146 Freedom Square <br /> Mandela Drive,CPT 95678
-            </h4>
-            <p>
-              Phone: 916.358.5622 <br />
-              Open hours <br />
-              Mon - Fri: 7:00 am - 8:00 pm <br />
-              Sat: 8:00 am - 3:00 pm <br />
-              Sun: Closed
-            </p>
-          </div>
-          <div className="call-two">
-            <a href="Button" className="click">
-              REQUEST A CALLBACK
-            </a>
-          </div>
-          <div className="call-three">
-            <a href="button" className="click-up">
-              Contact us
-            </a>
-          </div>
+        <div className="google-maps">
+          <MapWithAMarker
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAXOQcxZzrxgrzdLhtRcmdBaiPEWzFkD8"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `750px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </div>
-      </div>
-      <div className="google-maps">
-        <MapWithAMarker
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAXOQcxZzrxgrzdLhtRcmdBaiPEWzFkD8"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `750px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
       </div>
     </section>
   );
