14d7adb873a135e8637dab4a023e713099975941
Roche001 <rocheharold13@gmail.com>
2022-06-07 00:03:08 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -12,91 +12,87 @@ const Top = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <nav className="navbar">
-              <ul className="nav-items">
-                <Nav.Link>
-                  {" "}
-                  <Link
-                    to="/"
-                    className="link"
-                    onClick={() => setActiveNav("/")}
-                    id={activeNav === "/" ? "active" : ""}
-                  >
-                    <li>HOME</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/About"
-                    className="link"
-                    onClick={() => setActiveNav("/About")}
-                    id={activeNav === "/About" ? "active" : ""}
-                  >
-                    <li>ABOUT</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Services"
-                    className="link"
-                    onClick={() => setActiveNav("/Services")}
-                    id={activeNav === "/Services" ? "active" : ""}
-                  >
-                    <li>SERVICES</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Logistics"
-                    className="link"
-                    onClick={() => setActiveNav("/Logistics")}
-                    id={activeNav === "/Logistics" ? "active" : ""}
-                  >
-                    <li>LOGISTICS</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Career"
-                    className="link"
-                    onClick={() => setActiveNav("/Career")}
-                    id={activeNav === "/Career" ? "active" : ""}
-                  >
-                    <li>CAREER</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Team"
-                    className="link"
-                    onClick={() => setActiveNav("/Team")}
-                    id={activeNav === "/Team" ? "active" : ""}
-                  >
-                    <li>TEAM </li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Price"
-                    className="link"
-                    onClick={() => setActiveNav("/Price")}
-                    id={activeNav === "/Price" ? "active" : ""}
-                  >
-                    <li>PRICE</li>
-                  </Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link
-                    to="/Location"
-                    className="link"
-                    onClick={() => setActiveNav("/Location")}
-                    id={activeNav === "/Location" ? "active" : ""}
-                  >
-                    <li>LOCATION</li>
-                  </Link>
-                </Nav.Link>
-              </ul>
-            </nav>
+            <Nav.Link>
+              {" "}
+              <Link
+                className="link"
+                to="/"
+                onClick={() => setActiveNav("/")}
+                id={activeNav === "/" ? "active" : ""}
+              >
+                <li>HOME</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/About"
+                onClick={() => setActiveNav("/About")}
+                id={activeNav === "/About" ? "active" : ""}
+              >
+                <li>ABOUT</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Services"
+                onClick={() => setActiveNav("/Services")}
+                id={activeNav === "/Services" ? "active" : ""}
+              >
+                <li>SERVICES</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Logistics"
+                onClick={() => setActiveNav("/Logistics")}
+                id={activeNav === "/Logistics" ? "active" : ""}
+              >
+                <li>LOGISTICS</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Career"
+                onClick={() => setActiveNav("/Career")}
+                id={activeNav === "/Career" ? "active" : ""}
+              >
+                <li>CAREER</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Team"
+                onClick={() => setActiveNav("/Team")}
+                id={activeNav === "/Team" ? "active" : ""}
+              >
+                <li>TEAM </li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Price"
+                onClick={() => setActiveNav("/Price")}
+                id={activeNav === "/Price" ? "active" : ""}
+              >
+                <li>PRICE</li>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="link"
+                to="/Location"
+                onClick={() => setActiveNav("/Location")}
+                id={activeNav === "/Location" ? "active" : ""}
+              >
+                <li>LOCATION</li>
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
