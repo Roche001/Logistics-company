d6ba7533ab641c755997573b1427a5719354765c
Roche001 <rocheharold13@gmail.com>
2022-06-06 23:29:00 +0300

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/nav/Nav";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
