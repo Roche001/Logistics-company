ab54309e947b8044496484d66b5550bfa9333eb2
Roche001 <rocheharold13@gmail.com>
2022-06-15 23:23:02 +0300

    update changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,12 +26,11 @@ const About = () => {
               <h3>0.1 GLOBAL</h3>
               <h5>We are distributing our services across the globe</h5>
               <p>
-                We span major hubs on all continents providing a <br />
-                seamless supply chain solution focused on quality <br />
-                systems throughout. We operate strategically <br />
-                located transportation, distribution <br />
-                and value-added service centers <br /> throughout North America,
-                Europe and Asia.
+                We span major hubs on all continents providing a seamless supply
+                chain solution focused on quality systems throughout. We operate
+                strategically located transportation, distribution and
+                value-added service centers throughout North America, Europe and
+                Asia.
               </p>
             </div>
           </div>
@@ -44,12 +43,11 @@ const About = () => {
               <h3>0.2 VISION</h3>
               <h5>We have a unique philosophy and protect it</h5>
               <p>
-                Our culture is tuned to exceeding every customer’s <br />
-                expectations while providing a place for employees
-                <br /> to build their careers and prosper. Our environment
-                <br /> breeds success, and you’ll notice our people move
-                <br /> faster, work harder and are better rewarded than <br />
-                the competition.
+                Our culture is tuned to exceeding every customer’s expectations
+                while providing a place for employees to build their careers and
+                prosper. Our environment breeds success, and you’ll notice our
+                people move faster, work harder and are better rewarded than the
+                competition.
               </p>
             </div>
           </div>
@@ -63,13 +61,10 @@ const About = () => {
               <h5>We are adding value to each step of your supply chain</h5>
               <p>
                 Our job is to make sure that from raw material to finished
-                <br />
                 goods, to returns management, we provide the critical services
-                <br />
                 and information necessary to We then build a solution customized
-                <br /> to your cost and service requirements using all possible
-                information <br />
-                and our proven experience.
+                to your cost and service requirements using all possible
+                information and our proven experience.
               </p>
             </div>
           </div>
