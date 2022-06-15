fd93dc786984cacb4dbc141bbcac26c0adffb813
Roche001 <rocheharold13@gmail.com>
2022-06-15 23:32:53 +0300

    update changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -76,28 +76,27 @@ const About = () => {
             <h3>Our Organization at glance</h3>
           </div>
         </div>
-        <div className="row">
-          <div className="glance-about">
-            <div className="glance-about-img">
-              <img src="./assets/logo2.png" alt="glance" />
-            </div>
-            <div className="glance-about-para">
-              <p>
-                Our transportation company with 30 years of experience is you
-                best choice for shipping cargo of any size, storage, packing or
-                delivering wares to your customers. Our professional employees
-                will take care of your goods, whenever you send them. You are
-                granted complete control over the process of delivery by phone
-                or by our mobile app. Your freight is tracked every step of the
-                way. We provide a high standard of shipping, regardless of its
-                volume. If your company needs to establish a supply chain, we
-                have prepared several readymade solutions with flexible pricing
-                rates for you. Our urgent cargo services offer defined time
-                frames for convenience of your business. Our supply chain
-                services include shipping, warehousing, packaging, quality
-                control and distribution.
-              </p>
-            </div>
+
+        <div className="glance-about row">
+          <div className="glance-about-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <img src="./assets/logo2.png" alt="glance" />
+          </div>
+          <div className="glance-about-para col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>
+              Our transportation company with 30 years of experience is you best
+              choice for shipping cargo of any size, storage, packing or
+              delivering wares to your customers. Our professional employees
+              will take care of your goods, whenever you send them. You are
+              granted complete control over the process of delivery by phone or
+              by our mobile app. Your freight is tracked every step of the way.
+              We provide a high standard of shipping, regardless of its volume.
+              If your company needs to establish a supply chain, we have
+              prepared several readymade solutions with flexible pricing rates
+              for you. Our urgent cargo services offer defined time frames for
+              convenience of your business. Our supply chain services include
+              shipping, warehousing, packaging, quality control and
+              distribution.
+            </p>
           </div>
         </div>
       </div>
