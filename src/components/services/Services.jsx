622c2c000981389c6580b25d06f2eafeb6ad9e93
Roche001 <rocheharold13@gmail.com>
2022-06-19 06:06:43 +0300

    update changes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -39,152 +39,150 @@ const Services = () => {
           </div>
         </div>
 
-        <div className="offerings-container ">
-          <div className="offering-container-one row">
-            <div
-              className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
-              data-aos="fade-right"
-            >
-              <img
-                className="img-responsive"
-                src="./assets/road1.jpeg"
-                alt="road"
-              />
-            </div>
-            <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <h3>ROAD TRANSPORTATION</h3>
-              <p>
-                Delivery of full load, part load and group page shipments.
-                <br />
-                We will always find room on our trucks for your cargo.
-              </p>
-              <small>
-                We always handle your single shipments and your frequent
-                deliveries <br /> with equal amount of professionalism and
-                commitment. In our day-to-day <br />
-                operations, we constantly work on making our procedures more
-                efficient and <br />
-                reducing the strain on the environment without compromising
-                quality and service.
-              </small>
-              <div className="btn">Read More</div>
-            </div>
+        <div className="offering-container-one row">
+          <div
+            className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
+            data-aos="fade-right"
+          >
+            <img
+              className="img-responsive"
+              src="./assets/road1.jpeg"
+              alt="road"
+            />
           </div>
-          <div className="offering-container-one row">
-            <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <h3>SEA FREIGHT</h3>
-              <p>
-                Our company’s sea services provide much more than the average
-                logistics company. <br /> Sea is faster, safer.
-              </p>
-              <small>
-                We always handle your single shipments and your frequent
-                deliveries <br /> with equal amount of professionalism and
-                commitment. In our day-to-day <br />
-                operations, we constantly work on making our procedures more
-                efficient and <br />
-                reducing the strain on the environment without compromising
-                quality and service.
-              </small>
-              <div className="btn">Read More</div>
-            </div>
-            <div
-              className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
-              data-aos="fade-left"
-            >
-              <img
-                className="img-responsive"
-                src="./assets/road2.jpeg"
-                alt="road"
-              />
-            </div>
+          <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3>ROAD TRANSPORTATION</h3>
+            <p>
+              Delivery of full load, part load and group page shipments.
+              <br />
+              We will always find room on our trucks for your cargo.
+            </p>
+            <small>
+              We always handle your single shipments and your frequent
+              deliveries <br /> with equal amount of professionalism and
+              commitment. In our day-to-day <br />
+              operations, we constantly work on making our procedures more
+              efficient and <br />
+              reducing the strain on the environment without compromising
+              quality and service.
+            </small>
+            <div className="btn">Read More</div>
           </div>
-          <div className="offering-container-one row">
-            <div
-              className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
-              data-aos="fade-right"
-            >
-              <img
-                className="img-responsive"
-                src="./assets/road3.jpeg"
-                alt="road"
-              />
-            </div>
-            <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <h3>AIR FREIGHT</h3>
-              <p>
-                Not all cargo can travel by road. <br />
-                When you need to make it fly, we make it fly for you.
-              </p>
-              <small>
-                We always handle your single shipments and your frequent
-                deliveries <br /> with equal amount of professionalism and
-                commitment. In our day-to-day <br />
-                operations, we constantly work on making our procedures more
-                efficient and <br />
-                reducing the strain on the environment without compromising
-                quality and service.
-              </small>
-              <div className="btn">Read More</div>
-            </div>
+        </div>
+        <div className="offering-container-one row">
+          <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3>SEA FREIGHT</h3>
+            <p>
+              Our company’s sea services provide much more than the average
+              logistics company. <br /> Sea is faster, safer.
+            </p>
+            <small>
+              We always handle your single shipments and your frequent
+              deliveries <br /> with equal amount of professionalism and
+              commitment. In our day-to-day <br />
+              operations, we constantly work on making our procedures more
+              efficient and <br />
+              reducing the strain on the environment without compromising
+              quality and service.
+            </small>
+            <div className="btn">Read More</div>
           </div>
-          <div className="offering-container-one row">
-            <div className="offering-container-text  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <h3>LOGISTICS SOLUTIONS</h3>
-              <p>
-                Our company’s sea services provide much more than the average
-                logistics company. <br /> Sea is faster, safer.
-              </p>
-              <small>
-                We always handle your single shipments and your frequent
-                deliveries <br /> with equal amount of professionalism and
-                commitment. In our day-to-day <br />
-                operations, we constantly work on making our procedures more
-                efficient and <br />
-                reducing the strain on the environment without compromising
-                quality and service.
-              </small>
-              <div className="btn">Read More</div>
-            </div>
-            <div
-              className="offering-container-img  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
-              data-aos="fade-left"
-            >
-              <img
-                className="img-responsive"
-                src="./assets/road4.jpeg"
-                alt="road"
-              />
-            </div>
+          <div
+            className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
+            data-aos="fade-left"
+          >
+            <img
+              className="img-responsive"
+              src="./assets/road2.jpeg"
+              alt="road"
+            />
           </div>
-          <div className="offering-container-one row">
-            <div
-              className="offering-container-img  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
-              data-aos="fade-right"
-            >
-              <img
-                className="img-responsive"
-                src="./assets/road5.jpeg"
-                alt="road"
-              />
-            </div>
-            <div className="offering-container-text  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <h3>LOGISTICS SOLUTIONS</h3>
-              <p>
-                Not all cargo can travel by road. <br />
-                When you need to make it fly, we make it fly for you.
-              </p>
-              <small>
-                We always handle your single shipments and your frequent
-                deliveries <br /> with equal amount of professionalism and
-                commitment. In our day-to-day <br />
-                operations, we constantly work on making our procedures more
-                efficient and <br />
-                reducing the strain on the environment without compromising
-                quality and service.
-              </small>
-              <div className="btn">Read More</div>
-            </div>
+        </div>
+        <div className="offering-container-one row">
+          <div
+            className="offering-container-img col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
+            data-aos="fade-right"
+          >
+            <img
+              className="img-responsive"
+              src="./assets/road3.jpeg"
+              alt="road"
+            />
+          </div>
+          <div className="offering-container-text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3>AIR FREIGHT</h3>
+            <p>
+              Not all cargo can travel by road. <br />
+              When you need to make it fly, we make it fly for you.
+            </p>
+            <small>
+              We always handle your single shipments and your frequent
+              deliveries <br /> with equal amount of professionalism and
+              commitment. In our day-to-day <br />
+              operations, we constantly work on making our procedures more
+              efficient and <br />
+              reducing the strain on the environment without compromising
+              quality and service.
+            </small>
+            <div className="btn">Read More</div>
+          </div>
+        </div>
+        <div className="offering-container-one row">
+          <div className="offering-container-text  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3>LOGISTICS SOLUTIONS</h3>
+            <p>
+              Our company’s sea services provide much more than the average
+              logistics company. <br /> Sea is faster, safer.
+            </p>
+            <small>
+              We always handle your single shipments and your frequent
+              deliveries <br /> with equal amount of professionalism and
+              commitment. In our day-to-day <br />
+              operations, we constantly work on making our procedures more
+              efficient and <br />
+              reducing the strain on the environment without compromising
+              quality and service.
+            </small>
+            <div className="btn">Read More</div>
+          </div>
+          <div
+            className="offering-container-img  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
+            data-aos="fade-left"
+          >
+            <img
+              className="img-responsive"
+              src="./assets/road4.jpeg"
+              alt="road"
+            />
+          </div>
+        </div>
+        <div className="offering-container-one row">
+          <div
+            className="offering-container-img  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"
+            data-aos="fade-right"
+          >
+            <img
+              className="img-responsive"
+              src="./assets/road5.jpeg"
+              alt="road"
+            />
+          </div>
+          <div className="offering-container-text  col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h3>LOGISTICS SOLUTIONS</h3>
+            <p>
+              Not all cargo can travel by road. <br />
+              When you need to make it fly, we make it fly for you.
+            </p>
+            <small>
+              We always handle your single shipments and your frequent
+              deliveries <br /> with equal amount of professionalism and
+              commitment. In our day-to-day <br />
+              operations, we constantly work on making our procedures more
+              efficient and <br />
+              reducing the strain on the environment without compromising
+              quality and service.
+            </small>
+            <div className="btn">Read More</div>
           </div>
         </div>
       </div>
